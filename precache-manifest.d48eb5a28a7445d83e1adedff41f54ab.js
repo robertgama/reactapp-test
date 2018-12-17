10e0ef6a4f8a2443d1e99d196f98bbdc3589ae11
Robert Gama <robert.gamma@gmail.com>
2018-12-17 11:46:34 -0200

    Updates

---
+++ precache-manifest.d48eb5a28a7445d83e1adedff41f54ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b465ae6486f5f26fd744",
+    "url": "/reactapp-test/static/js/runtime~main.b465ae64.js"
+  },
+  {
+    "revision": "ddbbeb4f3829948d145b",
+    "url": "/reactapp-test/static/js/main.ddbbeb4f.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/reactapp-test/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "ddbbeb4f3829948d145b",
+    "url": "/reactapp-test/static/css/main.d85029f8.chunk.css"
+  },
+  {
+    "revision": "e390009fa65975bfca9d9fe79a9f9f95",
+    "url": "/reactapp-test/index.html"
+  }
+];
